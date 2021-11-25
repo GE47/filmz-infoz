737e3aff0f65bd53089da38e5201916212528b35
GE47 <hassan2pac47@gmail.com>
2021-11-25 14:03:19 +0300

    feat(*): remove outline foucs from chakraUI clickable elements

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { Provider } from "react-redux";
 import "@brainhubeu/react-carousel/lib/style.css";
 import "@fontsource/poppins";
+import "focus-visible/dist/focus-visible";
 
 import App from "./App";
 import theme from "./theme/index";
