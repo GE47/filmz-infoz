4d00a6d79b9034ad50b7b703a8522cb362e85a95
GE47 <hassan2pac47@gmail.com>
2021-11-27 23:07:25 +0300

    feat(Footer): build the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,38 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Divider, Stack, Text } from "@chakra-ui/react";
+import { GoMarkGithub } from "react-icons/go";
+import { FiLinkedin } from "react-icons/fi";
 
 const Footer = () => {
-  return <Box>Footer</Box>;
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <Box>
+      <Divider mb={5} mt={3} />
+
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        flexDir="column"
+      >
+        <Stack direction="row" spacing={5}>
+          <a href="https://github.com/GE47" target="_blank" rel="noreferrer">
+            <GoMarkGithub size="30px" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/alhasan-ali"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FiLinkedin size="30px" />
+          </a>
+        </Stack>
+        <Text fontSize="15px" mt="5px">
+          <b>@{currentYear}</b> Filmz Infoz
+        </Text>
+      </Box>
+    </Box>
+  );
 };
 
 export default Footer;
