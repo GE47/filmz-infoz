a5161859efb12c67b851174f7814a5076912cbb2
GE47 <hassan2pac47@gmail.com>
2021-12-22 14:12:33 +0300

    feat(Home): add Carousel and sliders to the home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,75 @@
+import { Box } from "@chakra-ui/react";
+import MainCarousel from "../components/Carousel/MainCarousel";
+import { useState, useEffect } from "react";
+
+import Slider from "../components/Slider";
+import MovieCard from "../components/Card/MovieCard";
+import LoadingIndicator from "../components/LoadingIndicator";
+
+type data = {
+  id: number;
+  url: string;
+};
+
 const Home = () => {
-  return <div>Home Page</div>;
+  const [images, setImages] = useState<data[] | null>(null);
+
+  const fetchImages = async () => {
+    try {
+      const request = await fetch(
+        "https://jsonplaceholder.typicode.com/albums/1/photos"
+      );
+
+      const response: data[] = await request.json();
+
+      const result = response.filter((_, index) => index < 8);
+
+      setImages(
+        result.map((data) => {
+          return { id: data.id, url: data.url };
+        })
+      );
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    fetchImages();
+  }, []);
+
+  if (!images) return <LoadingIndicator />;
+
+  return (
+    <Box>
+      <MainCarousel items={images} />
+      <Slider title="Popular Movies">
+        {images.map((image) => (
+          <span key={image.id}>
+            <MovieCard
+              name="movie name"
+              id={image.id}
+              rating={5}
+              imageUrl={image.url}
+            />
+          </span>
+        ))}
+      </Slider>
+
+      <Slider title="Top rated Movies">
+        {images.map((image) => (
+          <span key={image.id}>
+            <MovieCard
+              name="movie name"
+              id={image.id}
+              rating={8}
+              imageUrl={image.url}
+            />
+          </span>
+        ))}
+      </Slider>
+    </Box>
+  );
 };
 
 export default Home;
