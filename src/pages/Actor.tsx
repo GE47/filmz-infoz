e6f672dee045d76a5e1f76a0a25f192246fa1be2
GE47 <hassan2pac47@gmail.com>
2022-01-01 22:03:52 +0300

    feat(Acotr): add slider and actor detail card

--- src/pages/Actor.tsx
+++ src/pages/Actor.tsx
@@ -1,8 +1,39 @@
 import { useParams } from "react-router-dom";
+
+import ActorDetailCard from "../components/DetailCards/ActorDetailCard";
+import Slider from "../components/Slider";
+import MovieCard from "../components/Card/MovieCard";
+
 const Actor: React.FC = () => {
   const { id } = useParams();
 
-  return <h1>Actor {id}</h1>;
+  return (
+    <div>
+      <ActorDetailCard
+        bio="something"
+        name="Jane"
+        gender={1}
+        birthday="1999-9-9"
+        deathday="2034-29-2"
+        imageUrl=""
+      />
+
+      <Slider title="Participated In">
+        <span>
+          <MovieCard name="movie name" id="10" rating={8} imageUrl="" />
+        </span>
+        <span>
+          <MovieCard name="movie name" id="11" rating={8} imageUrl="" />
+        </span>
+        <span>
+          <MovieCard name="movie name" id="12" rating={8} imageUrl="" />
+        </span>
+        <span>
+          <MovieCard name="movie name" id="13" rating={8} imageUrl="" />
+        </span>
+      </Slider>
+    </div>
+  );
 };
 
 export default Actor;
