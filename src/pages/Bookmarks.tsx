8d9539db0bbba3831cf63218a74f6802be267ee7
GE47 <hassan2pac47@gmail.com>
2022-01-03 16:47:48 +0300

    feat(Bookmarks): build bookmarks page

--- src/pages/Bookmarks.tsx
+++ src/pages/Bookmarks.tsx
@@ -1,5 +1,25 @@
+import GridContainer from "../components/GridContainer";
+import MovieCard from "../components/Card/MovieCard";
+
 const Bookmarks = () => {
-  return <div>Bookmarks</div>;
+  return (
+    <GridContainer title="Bookmarks">
+      <MovieCard id="2" name="movie name" rating={8.7} imageUrl="" />
+      <MovieCard id="3" name="movie name" rating={8.7} imageUrl="" />
+      <MovieCard id="4" name="movie name" rating={8.7} imageUrl="" />
+      <MovieCard id="5" name="movie name" rating={8.7} imageUrl="" />
+      <MovieCard id="6" name="movie name" rating={8.7} imageUrl="" />
+      <MovieCard id="7" name="movie name" rating={8.7} imageUrl="" />
+      <MovieCard id="8" name="movie name" rating={8.7} imageUrl="" />
+      <MovieCard id="9" name="movie name" rating={8.7} imageUrl="" />
+      <MovieCard id="10" name="movie name" rating={8.7} imageUrl="" />
+      <MovieCard id="11" name="movie name" rating={8.7} imageUrl="" />
+      <MovieCard id="12" name="movie name" rating={8.7} imageUrl="" />
+      <MovieCard id="13" name="movie name" rating={8.7} imageUrl="" />
+      <MovieCard id="14" name="movie name" rating={8.7} imageUrl="" />
+      <MovieCard id="15" name="movie name" rating={8.7} imageUrl="" />
+    </GridContainer>
+  );
 };
 
 export default Bookmarks;
