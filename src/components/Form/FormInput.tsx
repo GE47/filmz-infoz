5404d105161fd2d5d5b4ea1957bb884114a99d54
GE47 <hassan2pac47@gmail.com>
2022-01-03 16:40:01 +0300

    feat(Form): create  FormInput component

---
+++ src/components/Form/FormInput.tsx
@@ -0,0 +1,60 @@
+import {
+  Box,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
+  FormControl,
+  FormLabel,
+  Text,
+} from "@chakra-ui/react";
+import { FaTimes } from "react-icons/fa";
+import { ReactJSXElement } from "@emotion/react/types/jsx-namespace";
+import { Field } from "formik";
+
+interface IProps {
+  leftIcon?: ReactJSXElement | undefined;
+  errorMessage: string | undefined;
+  placeholder?: string | undefined;
+  type?: string | undefined;
+  name: string | undefined;
+  label: string;
+}
+
+const FormInput: React.FC<IProps> = ({
+  leftIcon,
+  errorMessage,
+  type,
+  placeholder,
+  name,
+  label,
+}) => {
+  return (
+    <Box>
+      <FormControl>
+        <FormLabel>{label} :</FormLabel>
+        <InputGroup>
+          <InputLeftElement pointerEvents="none" children={leftIcon} />
+
+          <Input
+            as={Field}
+            name={name}
+            variant="primary"
+            type={type}
+            placeholder={placeholder}
+          />
+
+          <InputRightElement
+            pointerEvents="none"
+            children={errorMessage ? <FaTimes color="#E53E3E" /> : undefined}
+          />
+        </InputGroup>
+      </FormControl>
+      <Text py="5px" h="10px" color="red.500">
+        {errorMessage}
+      </Text>
+    </Box>
+  );
+};
+
+export default FormInput;
