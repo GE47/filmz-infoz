e4ebcbcbb230f5c21ce738a0bb5057508c26c458
GE47 <hassan2pac47@gmail.com>
2022-01-20 19:31:03 +0300

    fix(Slider): render only visible items

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -6,7 +6,7 @@ const Slider: React.FC<{ title: string }> = ({ children, title }) => {
     <Box pt={2}>
       <Heading>{title}</Heading>
       <Box p={2} bg="gray.700" borderRadius="md">
-        <Flicking align="prev" bound={true} horizontal={true}>
+        <Flicking align="prev" bound={true} horizontal={true} renderOnlyVisible>
           {children}
         </Flicking>
       </Box>
