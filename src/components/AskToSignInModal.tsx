939cad5f919382fb918c8c5bd14ff782195c81bf
GE47 <hassan2pac47@gmail.com>
2022-01-23 13:33:21 +0300

    feat: create a modal component that asks the user to sing in in order to add bookmarks

---
+++ src/components/AskToSignInModal.tsx
@@ -0,0 +1,59 @@
+import {
+  Box,
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+} from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
+interface IProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const AskToSignInModal: React.FC<IProps> = ({ isOpen, onClose }) => {
+  return (
+    <Modal size="sm" isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Sign In</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          Please sing in to add movies to your bookmark list
+        </ModalBody>
+        <ModalFooter
+          display="flex"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Box display="flex" style={{ gap: "5px" }}>
+            <Button
+              as={Link}
+              to="/signin"
+              fontSize={{ base: "13px", md: "16px" }}
+            >
+              Sing In
+            </Button>
+            <Button
+              as={Link}
+              to="/register"
+              fontSize={{ base: "13px", md: "16px" }}
+            >
+              Register
+            </Button>
+          </Box>
+          <Button onClick={onClose} fontSize={{ base: "13px", md: "16px" }}>
+            Cancel
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default AskToSignInModal;
