bc5be088b26f8950c6eed8f59fac7979a9beff2e
GE47 <hassan2pac47@gmail.com>
2022-01-23 13:34:57 +0300

    feat(Bookmark): handle adding and removing bookmarks and add AskToSignInModal

--- src/components/Bookmark.tsx
+++ src/components/Bookmark.tsx
@@ -1,25 +1,77 @@
-import { Box } from "@chakra-ui/react";
-import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Box, Button, useDisclosure } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 import { RiHeartAddLine, RiHeartAddFill } from "react-icons/ri";
+import {
+  addMovieBookmark,
+  BookmarkProps,
+  selectBookmarks,
+  removeMovieBookmark,
+} from "../store/bookmarks/bookmarksSlice";
+import AskToSignInModal from "./AskToSignInModal";
+import { auth } from "../firebase";
 
-const Bookmark: React.FC<{ id: string | number }> = ({ id }) => {
+const Bookmark: React.FC<BookmarkProps> = ({ id, poster, rating, title }) => {
   const [isBookmarked, SetIsBookmarked] = useState(false);
+  const { onClose, onOpen, isOpen } = useDisclosure();
+
+  const dispatch = useDispatch();
+  const bookmarks = useSelector(selectBookmarks);
+
+  useEffect(() => {
+    if (bookmarks.data.find((bookmark) => bookmark.id === id)) {
+      SetIsBookmarked(true);
+    } else {
+      SetIsBookmarked(false);
+    }
+  }, [dispatch, bookmarks.data, id]);
 
   const handleBookmark = (
-    e: React.MouseEvent<HTMLDivElement, MouseEvent>
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
   ): void => {
-    SetIsBookmarked(!isBookmarked);
+    if (!auth.currentUser) {
+      onOpen();
+      e.stopPropagation();
+      return;
+    }
+
+    const userId = auth.currentUser.uid;
+
+    if (isBookmarked) {
+      dispatch(removeMovieBookmark({ id, poster, rating, title, userId }));
+    } else {
+      dispatch(
+        addMovieBookmark({
+          id,
+          poster,
+          rating,
+          title,
+          userId,
+        })
+      );
+    }
+
     e.stopPropagation();
   };
 
   return (
-    <Box cursor="pointer" onClick={(e) => handleBookmark(e)}>
+    <Button
+      as={Box}
+      cursor="pointer"
+      disabled={bookmarks.status === "loading" ? true : false}
+      onClick={(e) => handleBookmark(e)}
+      bg="none"
+      p="0"
+      m="0"
+      _hover={{ bg: "none" }}
+    >
       {isBookmarked ? (
         <RiHeartAddFill size="25px" />
       ) : (
         <RiHeartAddLine size="25px" />
       )}
-    </Box>
+      <AskToSignInModal isOpen={isOpen} onClose={onClose} />
+    </Button>
   );
 };
 
