2e8aa744810da5d380a11242dec55dde3d484fad
GE47 <alhassan.ali.alrubaye@gmail.com>
2023-05-26 15:45:28 +0300

    Refactor(DetailsContainer): Add gap prop to component

--- src/components/DetailCards/DetailsContainer.tsx
+++ src/components/DetailCards/DetailsContainer.tsx
@@ -1,12 +1,19 @@
 import { Box } from "@chakra-ui/react";
 
-const DetailsContainer: React.FC = ({ children }) => {
+const DetailsContainer = ({
+  children,
+  gap,
+}: {
+  children: React.ReactNode;
+  gap?: number | string | undefined | object;
+}) => {
   return (
     <Box
       display="flex"
       alignItems="center"
       minH={{ base: "full", md: "30vh" }}
       flexDir={{ base: "column", md: "row" }}
+      gap={gap}
     >
       {children}
     </Box>
